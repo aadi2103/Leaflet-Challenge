61fa5f8041472aa0beb72cbdaceaf0f2b058f782
Aditya Bhatnagar <adityabhatnagar2103@gmail.com>
2020-06-13 02:41:49 -0400

    Updated Commit

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,7 +4,7 @@ var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "light-v10",
-    accessToken: 'pk.eyJ1Ijoic2Fjb3NpODkiLCJhIjoiY2thZWo3ejZnMDVncjJybzQxc3BmNXM2byJ9.DfB337nrnIxAd0vYJjEflA'
+    accessToken: 'pk.eyJ1Ijoic2Fjb3NpODkiLCJhIjoiY2thZWo3ejZnMDVncjJybzQxc3BmNXM2byJ9.DfB337nrnIxAd0vYJjEfl'
 });
 
 
